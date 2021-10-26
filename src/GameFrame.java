08177e4581c7e3dcbdfb5d72082162d93ddc8651
JohnnatanTomaz <johnnatan.tomaz1@gmail.com>
2021-10-26 13:53:10 -0300

    Game Created

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -12,7 +12,6 @@ public class GameFrame extends JFrame {
         this.setVisible(true);
         this.setLocationRelativeTo(null);
 
-
     }
 
 
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,6 +3,5 @@ public class SnakeGame {
 
         GameFrame frame = new GameFrame();
 
-
     }
 }
